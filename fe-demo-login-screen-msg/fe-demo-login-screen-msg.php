c6a8d05f279918c459fb9936c489465923190959
Sal Ferrarello <sal@ironcodestudio.com>
2017-03-01 09:25:46 -0500

    Add Demo Login Screen Message plugin

---
+++ fe-demo-login-screen-msg/fe-demo-login-screen-msg.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Plugin Name: Iron Code Demo Login Screen Message
+ * Plugin URI: https://github.com/salcode/wordpress-filter-examples/tree/master/fe-demo-login-screen-msg
+ * Description: A demo using the login_message filter to add a message on the login screen.
+ * Version: 0.1.0
+ * Author: Sal Ferrarello
+ * Author URI: http://salferrarello.com/
+ * Text Domain: fe-demo-login-screen-msg
+ * Domain Path: /languages
+ *
+ * @package fe-demo-login-screen-msg
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+add_filter( 'login_message', 'fe_demo_login_message' );
+
+/**
+ * Prepend "Use your words wisely" to the string passed in as a argument.
+ *
+ * @param  string $str The message to be displayed on the WordPress login page, often this is a blank string.
+ * @return string The message wih "Use your words wisely" added to the beginning.
+ */
+function fe_demo_login_message( $str ) {
+	return '<h1>Use your words wisely</h1>' . $str;
+}
